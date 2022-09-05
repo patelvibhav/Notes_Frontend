f9f0861be1569be572685a0c713de4fc4219b3fa
VIBHAV PATEL <104097638+patelvibhav@users.noreply.github.com>
2022-09-06 00:25:34 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Notes from "./Notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Notes />
+      <Notes1 />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
