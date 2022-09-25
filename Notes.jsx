cfc977f12698d029264674da4b808de3e84edc9c
VIBHAV PATEL <104097638+patelvibhav@users.noreply.github.com>
2022-09-26 00:43:11 +0530

    Create Notes.jsx
    Funfact 1

---
+++ Notes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Funfact 1</h1>
+      <p>
+        The longest fingernails ever were over 28 feet in total. American Lee
+        Redmond started growing them in 1979 and set the record in 2008. Sadly,
+        she lost her nails in a car crash in 2009
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
