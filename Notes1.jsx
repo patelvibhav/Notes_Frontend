97927fca12a10b7e1f26f61d13b7cd56f08d7734
VIBHAV PATEL <104097638+patelvibhav@users.noreply.github.com>
2022-10-09 18:42:19 +0530

    Create Notes1.jsx

---
+++ Notes1.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Funfact 2</h1>
+      <p>
+        The platypus doesn't have a stomach at all: Their esophagus goes
+        straight to their intestines. (nationalgeographic.com)
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
