760323b76c1612f14ce415226009222249a52115
VIBHAV PATEL <104097638+patelvibhav@users.noreply.github.com>
2022-10-26 01:03:30 +0530

    Create Notes2.jsx

---
+++ Notes2.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>DSA </h1>
+      <p>
+        Data structure refers to the way data is organized and manipulated. It
+        seeks to find ways to make data access more efficient. When dealing with
+        the data structure, we not only focus on one piece of data but the
+        different set of data and how they can relate to one another in an
+        organized manner.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
